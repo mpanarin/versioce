b6033261878095822193750bc445b244008694fc
mpanarin <mykhailopanarin@gmail.com>
2020-09-28 12:28:47 +0300

    :sparkles: add simple test for bump.version task

--- test/versioce_test.exs
+++ test/versioce_test.exs
@@ -1,7 +1,14 @@
 defmodule VersioceTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
-  test "greets the world" do
-    assert Versioce.hello() == :world
+  setup do
+    %{
+      current_version: Mix.Project.config()[:version]
+    }
+  end
+
+  test "Current version task", fixture do
+    assert capture_io(fn -> Mix.Task.run("bump.version") end) == fixture.current_version <> "\n"
   end
 end
