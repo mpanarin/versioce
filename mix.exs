cd82a24b768b0cbb9cfabe5ecfdc80ef08c58c59
Kian-Meng, Ang <kianmeng@cpan.org>
2020-09-28 20:10:13 +0800

    Clean up html doc generation

--- mix.exs
+++ mix.exs
@@ -1,22 +1,22 @@
 defmodule Versioce.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/mpanarin/versioce"
+  @version "0.1.0"
+
   def project do
     [
       app: :versioce,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: [{:ex_doc, "~> 0.21", only: [:release, :dev]}],
+      deps: deps(),
       description: description(),
       package: package(),
       # Docs
       name: "Versioce",
-      source_url: "https://github.com/mpanarin/versioce",
-      docs: [
-        main: "readme",
-        extras: ["README.md"]
-      ]
+      source_url: @source_url,
+      docs: docs()
     ]
   end
 
@@ -31,12 +31,28 @@ defmodule Versioce.MixProject do
     Versioce provides a mix task for version bumping of your project.
     """
   end
+
   def package do
     [
       name: :versioce,
       maintainers: ["Mykhailo Panarin"],
       licenses: ["GPL-3.0-or-later"],
-      links: %{"GitHub" => "https://github.com/mpanarin/versioce"}
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.22", only: [:release, :dev]}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      extras: ["README.md"]
     ]
   end
 end
