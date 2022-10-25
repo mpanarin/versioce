55bd552d518108c5271872f25d29866ec6b0b978
mpanarin <mykhailopanarin@gmail.com>
2022-10-25 17:47:02 +0300

    :sparkles: DataGrabber behaviour now has to be able to retrieve a singular version

--- lib/Changelog/DataGrabbers/behaviour.ex
+++ lib/Changelog/DataGrabbers/behaviour.ex
@@ -8,4 +8,5 @@ defmodule Versioce.Changelog.DataGrabber do
   Gets data for changelog generation, returns `Versioce.Changelog.DataGrabber.Versions.t()` format.
   """
   @callback get_versions(unreleased_to :: String.t()) :: {:ok, Versions.t()} | {:error, any}
+  @callback get_version(version :: String.t()) :: {:ok, Versions.version()} | {:error, any}
 end
