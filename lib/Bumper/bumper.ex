d8e9d8e8a4a4f629db4dcd039242668c4af356ea
mpanarin <mykhailopanarin@gmail.com>
2024-03-13 15:59:30 +0200

    :bug: make sure bumping to the same version does nothing

--- lib/Bumper/bumper.ex
+++ lib/Bumper/bumper.ex
@@ -27,20 +27,34 @@ defmodule Versioce.Bumper do
   end
 
   @doc """
-  Bumps versions in all the files specified in config + mix.exs
+  Bumps versions in all the files specified in config.
 
   ## Example
 
-      iex> Versioce.Bumper.bump({[], ["minor"]}, "0.0.1")
+      iex> Versioce.Bumper.bump("0.0.1", "0.1.0")
       "0.1.0"
   """
-  @spec bump({OptionParser.parsed(), OptionParser.argv()}, String.t()) :: String.t()
-  def bump({[], []}, from) do
+  @spec bump(from :: String.t(), to :: String.t()) :: String.t()
+  def bump(from, to) do
+    Files.update_version_files(from, to)
+    to
+  end
+
+  @doc """
+  Get next project version based on options passed.
+
+  ## Example
+
+      iex> iex> Versioce.Bumper.get_new_version({[], ["minor"]}, "0.0.1")
+      "0.1.0"
+  """
+  @spec get_new_version({OptionParser.parsed(), OptionParser.argv()}, String.t()) :: String.t()
+  def get_new_version({[], []}, from) do
     IO.puts("Nothing to do")
     from
   end
 
-  def bump({options, ["patch"]}, from) do
+  def get_new_version({options, ["patch"]}, from) do
     from_v = Version.parse!(from)
 
     new_version =
@@ -51,12 +65,10 @@ defmodule Versioce.Bumper do
       |> to_string
 
     Version.parse!(new_version)
-
-    Files.update_version_files(from, new_version)
     new_version
   end
 
-  def bump({options, ["minor"]}, from) do
+  def get_new_version({options, ["minor"]}, from) do
     from_v = Version.parse!(from)
 
     new_version =
@@ -68,12 +80,10 @@ defmodule Versioce.Bumper do
       |> to_string
 
     Version.parse!(new_version)
-
-    Files.update_version_files(from, new_version)
     new_version
   end
 
-  def bump({options, ["major"]}, from) do
+  def get_new_version({options, ["major"]}, from) do
     from_v = Version.parse!(from)
 
     new_version =
@@ -86,38 +96,27 @@ defmodule Versioce.Bumper do
       |> to_string
 
     Version.parse!(new_version)
-
-    Files.update_version_files(from, new_version)
     new_version
   end
 
-  def bump({_options, ["calver"]}, from) do
+  def get_new_version({_options, ["calver"]}, _from) do
     today = Date.utc_today()
     format = Config.calver_format()
 
     {:ok, new_version} = CalverFormattingParser.parse_format(format, today)
-
-    Files.update_version_files(from, new_version)
     new_version
   end
 
-  def bump({_, [version]}, from) do
-    new_version = Version.parse!(version) |> to_string
-
-    Files.update_version_files(from, new_version)
-    new_version
+  def get_new_version({_, [version]}, _from) do
+    Version.parse!(version) |> to_string
   end
 
-  def bump({options, []}, from) do
+  def get_new_version({options, []}, from) do
     from_v = Version.parse!(from)
 
-    new_version =
-      from_v
-      |> add_build_pre(options)
-      |> to_string
-
-    Files.update_version_files(from, new_version)
-    new_version
+    from_v
+    |> add_build_pre(options)
+    |> to_string
   end
 
   # Adds 'build' and 'pre' values to the `Version` struct of they were passed in options.
